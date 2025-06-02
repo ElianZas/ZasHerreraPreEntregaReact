16991aade89248ff05a4fc69b0d1498bf5351e1f
ElianZas <elianzas905@gmail.com>
2025-06-02 03:52:26 -0300

    ajuste

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://ElianZas.github.io/25017ZasHerreraElian_PreEntrega"
+  base:"https://ElianZas.github.io/ZasHerreraPreEntregaReact"
 })
